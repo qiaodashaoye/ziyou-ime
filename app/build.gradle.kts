c5d771bbaf621ea3e3badb7418cfe60ab6843c09
乔大少爷 <1451449315@qq.com>
2026-07-21 08:01:50 +0800

    Add librime as git submodule and fix JNI CMake path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 
     externalNativeBuild {
         cmake {
-            path = file("src/main/jni/CMakeLists.txt")
+            path = file("src/main/jni/librime_jni/CMakeLists.txt")
             version = "3.22.1"
         }
     }
