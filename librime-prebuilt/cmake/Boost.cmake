90b437c800c0d8f8d7dec58eb56d12c7458a5928
qpg <1451449315@qq.com>
2026-07-23 13:37:13 +0800

    还原boost版本

--- librime-prebuilt/cmake/Boost.cmake
+++ librime-prebuilt/cmake/Boost.cmake
@@ -5,7 +5,7 @@
 # 下载并解压指定版本的 Boost（cmake 打包版），提供 header + 编译型库
 # （regex 等）。移植自 Trime 的 app/src/main/jni/cmake/Boost.cmake。
 
-set(BOOST_VERSION 1.90.0)
+set(BOOST_VERSION 1.89.0)
 
 # 父目录（librime-prebuilt/）是 Boost 下载和解压的位置
 set(PREBUILT_ROOT "${CMAKE_CURRENT_LIST_DIR}/..")
@@ -24,7 +24,7 @@ if(NOT EXISTS "${PREBUILT_ROOT}/boost-${BOOST_VERSION}.tar.xz")
       DOWNLOAD "${_url}"
       "${PREBUILT_ROOT}/boost-${BOOST_VERSION}.tar.xz"
       EXPECTED_HASH
-        SHA256=aca59f889f0f32028ad88ba6764582b63c916ce5f77b31289ad19421a96c555f
+        SHA256=67acec02d0d118b5de9eb441f5fb707b3a1cdd884be00ca24b9a73c995511f74
       SHOW_PROGRESS
       STATUS _dl_status)
     list(GET _dl_status 0 _dl_code)
